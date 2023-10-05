287b9d3efb9aa250d990fdb9a9b3aaa4b6fc7232
schr0dy <93306565+schr0dy@users.noreply.github.com>
2023-10-05 12:47:12 +0200

    First commit
    Added wordpress plugin.

---
+++ wpformtodiscord.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name: WPForms Discord Integration
+ * Description: Send WPForms entries to Discord webhook.
+ * Version: 1.0
+ * Author: vikarbonato
+ */
+
+add_action('wpforms_process_complete', 'send_wpforms_to_discord', 10, 4);
+
+function send_wpforms_to_discord($fields, $entry, $form_data, $entry_id) {
+    $webhook_url = 'YOUR WEBHOOK LINK HERE';
+	$message = "||@here||\n";
+    $message .= "*You got a new contact request:*\n";
+	$field_names = array();
+	$field_values = array();
+    // Storing data in arrays to use loops
+	foreach ($fields as $field) {
+		$field_names[] = $field['name'];
+		$field_values[] = $field['value'];
+	}
+    //The loop
+	for ($i = 0; $i < count($field_names); $i++) {
+		$message .= "**{$field_names[$i]}:** *{$field_values[$i]}*\n";
+	}
+    $data = [
+        'content' => $message,
+    ];
+
+    $response = wp_safe_remote_post($discord_webhook_url, [
+        'body' => json_encode($data),
+        'headers' => [
+            'Content-Type' => 'application/json',
+        ],
+    ]);
+
+    error_log(print_r($response, true));
+}
